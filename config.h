cbee3460db2cfab2683ae3228035cf2fc94e4460
Abhinav Prasai <abhinav.prsai@gmail.com>
2025-10-27 16:42:00 +0000

    add more config options

--- config.h
+++ config.h
@@ -1,3 +1,7 @@
 #pragma once
 
-#define PET_SMOOTH 10 /* smoothening factor for window movement */
+#include <X11/keysymdef.h>
+
+#define PET_SMOOTH  200 /* smoothening factor for window movement */
+#define PET_REFRESH 32 /* ms between movement updates (16ms=60fps) */
+#define CALL_KEY    "Alt_R"
